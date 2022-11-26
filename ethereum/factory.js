517ae2df20ee23952abb6040cb8ffc04dc36dfc4
Zartosht Sepideman <xashayarsha@gmail.com>
2022-11-26 18:16:20 +0300

    test add address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,8 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   CampaignFactory.abi,
-  process.env.NEXT_PUBLIC_FACTORY_CONTRACT_ADDRESS,
+  // process.env.NEXT_PUBLIC_FACTORY_CONTRACT_ADDRESS,
+  "0xFdb3e9e1801e7ef37D6c18234E988b8f8137428A",
 );
 
 export default instance;
